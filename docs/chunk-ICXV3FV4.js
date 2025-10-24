fb8561e910fb485014103e82bf4ad94006dc9088
yerzyrodriguez-hash <yerzy.rodriguez@uab.edu.bo>
2025-10-24 00:09:42 -0400

    feat:Buildeo del FrontEnd

---
+++ docs/chunk-ICXV3FV4.js
@@ -0,0 +1,1 @@
+import{La as a,Ma as n,Na as i,wa as o}from"./chunk-VZGSJCX6.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),i(1,"about works!"),n())},encapsulation:2})};export{s as About};
--- docs/chunk-ZOOWFZ73.js
+++
@@ -1,1 +0,0 @@
-import{Da as a,Ea as n,Ga as i,ra as o}from"./chunk-IHMA47CN.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),i(1,"about works!"),n())},encapsulation:2})};export{s as About};
